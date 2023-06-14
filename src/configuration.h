38370aab595b22c15bc94904e7237b38fbab88de
Jm Casler <jm@casler.org>
2023-06-13 21:51:08 -0700

    Able to set DMX colors for two RBG fixtures

--- src/configuration.h
+++ src/configuration.h
@@ -10,3 +10,10 @@
 #define BLOWER_DUTY_PIN 15 // Used to define the duty cycle of the blower to moderate voltage.
 
 
+#define DMX_DE 4 // Expansion A
+#define DMX_RE 5 // Expansion A
+
+#define DMX_RO 2 
+#define DMX_DI 4 
+
+#define DMX_NOVA_PACKET_SIZE 32
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <Arduino.h>
 #include "configuration.h"
 #include <Adafruit_MCP23X17.h>
+// #include <DmxSimple.h>
+#include <FastLED.h>
+#include "esp_dmx.h"
 
 Adafruit_MCP23X17 mcp_a;
+
+dmx_port_t dmxPort = 1;
+// byte data[DMX_PACKET_SIZE];
+byte data[DMX_PACKET_SIZE];
+unsigned long lastUpdate = millis();
+CRGB leds[2];
 
 void setup()
 {
@@ -10,14 +19,19 @@ void setup()
   delay(500); // Give time for the serial monitor to connect
   Serial.println("NOVA: STARBASE");
 
+  pinMode(BLOWER_DUTY_PIN, OUTPUT);
+
+  /* Set the DMX hardware pins to the pins that we want to use. */
+  dmx_set_pin(dmxPort, DMX_DI, DMX_RO, 0);
+
+  /* Now we can install the DMX driver! We'll tell it which DMX port to use and
+    which interrupt priority it should have. If you aren't sure which interrupt
+    priority to use, you can use the macro `DMX_DEFAULT_INTR_FLAG` to set the
+    interrupt to its default settings.*/
+  dmx_driver_install(dmxPort, DMX_DEFAULT_INTR_FLAGS);
+
   uint8_t blowerDuty = 250;
   float blowerVolt = ((blowerDuty / 255.0) * 24.0);
-
-  // pinMode(FOG_STATUS, INPUT);
-  // pinMode(FOG_POWER, OUTPUT);
-  // pinMode(FOG_ACTIVATE, OUTPUT);
-
-  pinMode(BLOWER_DUTY_PIN, OUTPUT);
 
   Serial.print("Blower: Setting up Blower Duty Pin (Max is 255) @ ");
   Serial.print(blowerDuty);
@@ -37,6 +51,16 @@ void setup()
     {
       // Do nothing
     };
+  }
+
+  Serial.println("DMX: Setting Pin States.");
+  if (1)
+  {
+    mcp_a.pinMode(DMX_DE, OUTPUT);
+    mcp_a.pinMode(DMX_RE, OUTPUT);
+
+    mcp_a.digitalWrite(DMX_DE, HIGH);
+    mcp_a.digitalWrite(DMX_RE, HIGH);
   }
 
   Serial.println("Fog Machine: Setting Pin States.");
@@ -76,5 +100,57 @@ void loop()
     Serial.println(" seconds.");
     mcp_a.digitalWrite(FOG_ACTIVATE, LOW);
     delay(fogRandomDelay * 1000);
+  }
+
+  /* Get the current time since boot in milliseconds so that we can find out how
+   long it has been since we last updated data and printed to the Serial
+   Monitor. */
+  unsigned long now = millis();
+  // leds[0].
+
+  if (now - lastUpdate >= 1000)
+  {
+    /* Increment every byte in our packet. Notice we don't increment the zeroeth
+      byte, since that is our DMX start code. Then we must write our changes to
+      the DMX packet. Maximum is kept in DMX_PACKET_SIZE */
+    data[0] = 0x00; // Null
+
+    // Channel 1
+    data[1] = 0xff; // Brightness
+    data[2] = 0x00; // Red
+    data[3] = 0xff; // Green
+    data[4] = 0x00; // Blue
+    data[5] = 0x00;
+    data[6] = 0x00;
+    data[7] = 0x00; // Null
+
+    // Channel 2
+    data[8] = 0xff;  // Brightness
+    data[9] = 0xff;  // Red
+    data[10] = 0x00; // Green
+    data[11] = 0xff; // Blue
+    data[12] = 0x00;
+    data[13] = 0x00;
+    data[14] = 0x00; // Null
+
+    dmx_write(dmxPort, data, DMX_PACKET_SIZE);
+
+    /* Log our changes to the Serial Monitor. */
+    Serial.printf("Sending DMX 0x%02X\n", data[1]);
+    lastUpdate = now;
+  }
+
+  /* Now we can transmit the DMX packet! */
+  dmx_send(dmxPort, DMX_PACKET_SIZE);
+
+  /* We can do some other work here if we want. */
+
+  /* If we have no more work to do, we will wait until we are done sending our
+    DMX packet. */
+  if (!dmx_wait_sent(dmxPort, DMX_TIMEOUT_TICK))
+  {
+    /* If we get here, it means that we timed out waiting for the DMX packet to
+      send. */
+    Serial.println("DMX: Timed out waiting for DMX packet to send.");
   }
 }
